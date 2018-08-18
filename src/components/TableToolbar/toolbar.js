a3d57211d871bead2a567bedb2fb47007be5af22
Dyachenko R <antinaar@gmail.com>
2018-08-18 21:35:38 +0300

    add tabletoolbar component

---
+++ src/components/TableToolbar/toolbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import Search from '../Search';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TableTitle = styled.h2`
+  font-size: 20px;
+  font-weight: normal;
+`;
+
+export default (props) => {
+  return (
+    <Wrapper>
+      <TableTitle>{props.title}</TableTitle>
+      <Search/>
+    </Wrapper>
+  )
+}
