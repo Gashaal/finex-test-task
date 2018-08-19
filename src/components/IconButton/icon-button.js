c8f68b8c9f2019070c86c927d252beaf679eb7ed
Dyachenko R <antinaar@gmail.com>
2018-08-19 14:35:57 +0300

    add IconButton components

---
+++ src/components/IconButton/icon-button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #eff0f3;
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
+const Icon = styled.img`
+  height: 20px;
+`;
+
+export default (props) => {
+  return (
+    <Button>
+      <Icon src={props.icon}/>
+    </Button>
+  )
+}
